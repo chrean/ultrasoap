3f3d4d64736638eddedb745bbc19247b303275df
Gabriel Sambarino <gabriel.sambarino@usablenet.com>
2014-06-18 16:10:25 +0000

    Rearranging settings into a new YAML file

--- lib/client.rb
+++ lib/client.rb
@@ -5,41 +5,68 @@ module UltraSOAP
 
   class Client
 
-    attr :logger
-    attr :logdest
-    attr :transaction_id
-    attr :use_transactions
-    attr :settings
+    # Class variables
+    @logger
+
+    @logdest            = './ultrasoap.log'
+    @logging            = false
+    @log_level          = 'error'
+    @test_wsdl          = 'https://testapi.ultradns.com/UltraDNS_WS/v01?wsdl'
+    @prod_wsdl          = 'https://ultra-api.ultradns.com/UltraDNS_WS/v01?wsdl'
+    @environment        = 'production'
+    @transaction_id     = nil
+    @use_transactions   = false
+
+    class << self
+      attr_accessor :logdest
+      attr_accessor :logging
+      attr_accessor :log_level
+      attr_accessor :transaction_id
+      attr_accessor :test_wsdl
+      attr_accessor :prod_wsdl
+      attr_accessor :environment
+      attr_accessor :use_transactions
+    end
+
+    # Instance variables
+    attr_accessor :logging
+    attr_accessor :loglevel
 
     # Constructor
-    def initialize(ultra_wsdl, username, password, use_transactions=false, logfile='./ultrasoap.log', logging=true, log_lev='debug')
-      # Mandatory parameters check
-      return nil unless (ultra_wsdl.nil? or username.nil? or password.nil?)
+    def initialize()
+      # Exits if username or password aren't specified.
+      return nil unless !(Settings.username.nil? or Settings.password.nil?)
 
-      @transaction_id = nil
+      username = Settings.username
+      password = Settings.password
 
-      # Setting up log destination and logger instance
-      @logdest = logfile
+      @environment = Settings['environment'] || Client.environment
 
-      @logger = Logger.new(logfile)
+      # We don't need the following to be instance variables
+      ultra_wsdl = (@environment == 'test' ? (Settings['test_wsdl'] || Client.test_wsdl) : (Settings['prod_wsdl'] || Client.prod_wsdl))
+
+      # Setting up parameter values, if provided. If not, defaults are assumed
+
+      @use_transactions = Settings['use_transactions'] || Client.use_transactions
+      @logdest = Settings['logfile'] || Client.logdest
+
+      @logger = Logger.new(@logdest)
+      @logging = Settings['logging'] || Client.logging
 
       begin
+
         @client = Savon.client do
           wsdl ultra_wsdl
           env_namespace 'soapenv'
           wsse_auth(username, password)
           element_form_default :unqualified
-          log logging
-          log_level log_lev.to_sym
+          log Settings['logging']
+          log_level Settings['log_level'].to_sym
           pretty_print_xml true
         end
-        
-        # Setting local variables
-        @use_transactions = use_transactions
-
       rescue Exception => e  
         @logger.error("Error in ultrasoap.initialize: " + e.message)
-        raise Error, 'Error in ultrasoap initialization, check log file'
+        raise 'Error in ultrasoap initialization, check log file'
       end
     end
 
@@ -102,6 +129,65 @@ module UltraSOAP
         trans_commit = self.send_request :commit_transaction, message
       rescue Exception => e
         @logger.error("Error in ultrasoap.transaction_commit: " + e.message)
+      end
+    end
+
+    # Helper method to retrieve load balancing pools data
+    # Parameters:
+    # - zone (don't forget the trailing dot)
+    def get_lb_pools(zone)
+      message = {
+        :zone_name    => zone,
+        :lb_pool_type => 'SB'
+      }
+
+      begin
+        response = self.send_request :get_load_balancing_pools_by_zone, message
+        return response
+      rescue Exception => e
+        @logger.error("Error in retrieving SB pools for the zone #{zone}: #{e.message}")
+        return nil
+      end
+    end
+
+    # Helper method to retrieve Pool's records
+    # Parameters:
+    # - poolID
+    def get_pool_records(pool_id)
+      message = {
+        :pool_id => pool_id.to_s
+      }
+
+      begin
+        return self.send_request :get_pool_records, message
+      rescue Exception => e
+        @logger.error("Error while retrieving Pool Records for the Pool ID #{pool_id.to_s}: #{e.message}")
+      end
+    end
+
+    # LOOKUP methods
+
+    def lookup_pool_type(pt_code)
+      case pt_code.to_s
+      when 'SB'
+        'SiteBacker'
+      when 'RD'
+        'Resource Distribution'
+      when 'TC'
+        'Traffic Controller'
+      else
+        'Other'
+      end
+    end
+
+    def lookup_response_method(rm_code)
+      case rm_code.to_s
+      when 'FX'
+        'Fixed'
+      when 'RR'
+        'Round Robin'
+      when 'RD'
+        'Random'
       end
     end
 
--- lib/settings.rb
+++ lib/settings.rb
@@ -1,6 +1,7 @@
-#log_file    : 'ultrasoap.log'
-#log         : false
-#log_level   : 'error'
-#test_wsdl   : 'https://testapi.ultradns.com/UltraDNS_WS/v01?wsdl'
-#prod_wsdl   : 'https://ultra-api.ultradns.com/UltraDNS_WS/v01?wsdl'
-#transactions: false
+require 'settingslogic'
+
+module UltraSOAP
+  class Settings < Settingslogic
+    source "#{ENV['HOME']}/.ultrasoap"
+  end
+end
